c6d0e6ed40544661d1a3ed2b9fa618dc484fc3b4
Krisno Mukti <ibkrsnmk@gmail.com>
2024-02-18 14:07:41 +0000

    quick re-org

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import About from './pages/About';
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<h1>hello, world!</h1>} />
-        <Route path='/about' element={<h1>hello, react router!</h1>} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
   );
