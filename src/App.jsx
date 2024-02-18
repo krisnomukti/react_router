90ae17d00c5eca2b45a8bfdaffff45a0c5c38572
Krisno Mukti <ibkrsnmk@gmail.com>
2024-02-18 15:16:25 +0000

    feat(vans): vans page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
+import Vans from './pages/Vans';
 
 import '../server/server';
 
@@ -15,12 +16,14 @@ export default function App() {
 
         <nav>
           <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
         </nav>
       </header>
 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+export default function Vans() {
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    const request = async () => {
+      const response = await fetch('/api/vans');
+      const data = await response.json();
+
+      setVans(data.vans);
+    };
+
+    request();
+  }, []);
+
+  const vanElements = vans.map(({ id, name, price, type, imageUrl }) => {
+    return (
+      <div className='van-tile' key={id}>
+        <img src={imageUrl} alt='Gambar' />
+
+        <div className='van-info'>
+          <h1>{name}</h1>
+
+          <p>
+            {price}
+            <span>/Day</span>
+          </p>
+        </div>
+
+        <i className={`van-type ${type} selected`}>{type}</i>
+      </div>
+    );
+  });
+
+  return (
+    <div className='van-list-container'>
+      <h1>Explore our van options</h1>
+
+      <div className='van-list'>{vanElements}</div>
+    </div>
+  );
+}
