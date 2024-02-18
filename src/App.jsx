2c2f3419f37459ee04392a3e07530ca2aded1ff6
Krisno Mukti <ibkrsnmk@gmail.com>
2024-02-18 14:14:22 +0000

    feat: link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import About from './pages/About';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </nav>
+
       <Routes>
         <Route path='/' element={<h1>hello, world!</h1>} />
         <Route path='/about' element={<About />} />
