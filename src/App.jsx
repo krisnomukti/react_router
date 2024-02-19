7792b0cba25d08c4284498c5f867dd8d52557ed2
Krisno Mukti <ibkrsnmk@gmail.com>
2024-02-19 00:34:46 +0000

    fix(Navbar/Layout): fixing the navbar with a layout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
 import Vans from './pages/Vans';
 import VansDetail from './pages/VansDetail';
+import Layout from './components/Layout';
 
 import '../server/server';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to='/' className='site-logo'>
-          #VANLIFE
-        </Link>
-
-        <nav>
-          <Link to='/about'>About</Link>
-          <Link to='/vans'>Vans</Link>
-        </nav>
-      </header>
-
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VansDetail />} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<Vans />} />
+          <Route path='/vans/:id' element={<VansDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header>
+      <Link to='/' className='site-logo'>
+        #VANLIFE
+      </Link>
+
+      <nav>
+        <Link to='/about'>About</Link>
+        <Link to='/vans'>Vans</Link>
+      </nav>
+    </header>
+  );
+}
