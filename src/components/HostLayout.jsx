e09c42ba4e302ce81f8a9df1fffef5231c461457
Krisno Mukti <ibkrsnmk@gmail.com>
2024-02-19 01:02:59 +0000

    feat: creating the host layout

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,9 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export default function HostLayout() {
   return (
-    <div>
+    <>
+      <nav className='host-nav'>
+        <Link to='/host'>Dashboard</Link>
+        <Link to='/host/income'>Income</Link>
+        <Link to='/host/reviews'>Reviews</Link>
+      </nav>
+
       <Outlet />
-    </div>
+    </>
   );
 }
